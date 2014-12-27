67a3aff0fcf5c68d783bdf0602dac752a405014c
Vadim Sikora <vadim.sikora@gmail.com>
2014-12-27 08:56:54 +0100

    jscs + jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
-var to5 = require('gulp-6to5');
+var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
 gulp.task('default', function () {
     return gulp.src('src/*.js')
-        .pipe(to5())
-        .pipe(gulp.dest('dist'));
+        .pipe(jscs()).on('error', function (error) {
+            gutil.log('\n' + error.message);
+        })
+        .pipe(jshint());
 });
 
 gulp.task('watch', function () {
