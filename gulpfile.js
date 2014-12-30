85590ab7523b1030bfe438a302dee1e918b72f5a
Vadim Sikora <vadim.sikora@gmail.com>
2014-12-30 09:36:51 +0100

    demo

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,27 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function () {
-    return gulp.src('src/*.js')
+    gulp.src('src/*.js')
         .pipe(jscs()).on('error', function (error) {
             gutil.log('\n' + error.message);
         })
         .pipe(jshint());
 });
 
+gulp.task('demo', function () {
+    gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ff > 24']
+        }))
+        .pipe(gulp.dest('css/'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('src/*.js', ['default']);
+    gulp.watch('scss/*.scss', ['demo']);
 });
